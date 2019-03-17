a4bd3b2425779f463ddb1e3c38b1ab3c278f78fc
Freddie Sackur <github@dustyfox.uk>
2019-03-17 13:07:27 +0000

    New ASP.NET MVC project

---
+++ Clubsy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clubsy.Startup))]
+namespace Clubsy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
