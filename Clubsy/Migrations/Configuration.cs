4f1ad196f48be08364313133b65132aea900e973
Freddie Sackur <github@dustyfox.uk>
2019-03-17 14:05:49 +0000

    Set local DB, enabled migrations

---
+++ Clubsy/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Clubsy.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Clubsy.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Clubsy.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Clubsy.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
