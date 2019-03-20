27c6d133bebd5c853585f0ecd5abbe9b7692d5b4
Freddie Sackur <github@dustyfox.uk>
2019-03-20 01:58:03 +0000

    Autocomplete on club search

--- Clubsy/App_Start/BundleConfig.cs
+++ Clubsy/App_Start/BundleConfig.cs
@@ -12,22 +12,17 @@ namespace Clubsy
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery-ui-{version}.js",
                         "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js",
+                        "~/Scripts/clubsy.js"));
+            
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/themes/base/jquery-ui.css",
---
+++ Clubsy/Scripts/clubsy.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+
+    var ajaxFormSubmit = function () {
+        var $form = $(this);
+
+        var options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+
+        $.ajax(options).done(function (data) {
+            var $target = $($form.attr("data-ajax-update"));
+            var $newHtml = $(data);
+            $target.replaceWith($newHtml);
+            $newHtml.effect("highlight");
+        });
+
+        return false;
+    };
+
+    var createAutoComplete = function () {
+        var $input = $(this);   // 'this' gets each matching DOM element matching "input[data_clubsy_autocomplete_action]"
+
+        var options = {
+            source: $input.attr("data_clubsy_autocomplete_action")
+        };
+
+        $input.autocomplete(options);
+    };
+
+    $("form[data-clubsy-filter='club']").submit(ajaxFormSubmit);
+    $("input[data_clubsy_autocomplete_action]").each(createAutoComplete);
+});
